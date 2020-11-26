1f2ea3584bd4fe8198b044b07bd4b10f81716587
Ryan Choe <ryan.choe@outlook.com>
2020-11-26 01:31:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,8 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto]
- :yukihiro_matsumoto = :known_for => "Ruby", 
-  :languages => ["LISP", "C"]
+ :yukihiro_matsumoto = {:known_for => "Ruby"} 
+ :yukihiro_matsumoto = {:languages => ["LISP", "C"]}
   
 }
 
